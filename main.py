1bd51f6d56c34327638d1e9f13cfc9cb252fb868
Yuri Numerov <achi@hush.ai>
2015-10-28 00:08:06 +0100

    that's it?

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 #! /usr/bin/env python
 
 import os
-from story import chapter1
-from story import chapter2
+for i in range(1,len(os.listdir('./story'))):exec('from story import chapter%i'%i)
 
 prev=0
 current=chapter1.a1
-choice=-1
+choice=0
 
 while 1:
-  if choice!=-1: print prev[1:][int(choice)-1][1]
+  if choice!=0: print prev[1:][int(choice)-1][1]
   os.system('clear')
   print current[0],"\n"
   print "\n".join([i[0] for i in current[1:]]) 
-  choice=raw_input() 
+  try: choice=int(raw_input())
+  except KeyboardInterrupt: exit()
+  except: pass
+  if choice in range(1,len(current[1:])+1):
+    prev=current
+    tmp=prev[1:][choice-1]
+    current=eval(tmp[2]+"."+tmp[3])
